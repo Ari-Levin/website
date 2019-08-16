94003adad8076a7861e58aa6a1ee7638af851f42
Ari-Levin <arisports@aol.com>
2019-08-16 13:09:12 -0400

    create poke script

---
+++ main.js
@@ -0,0 +1,14 @@
+
+	```
+	//define a function
+	function sayDontPoke() {
+	    alert('dont poke the fox');
+	}
+	
+	// register the function to the onclick event
+	document.querySelector('img').onclick = sayDontPoke
+	
+	// call when page loads
+	sayDontPoke()
+	```
+	
